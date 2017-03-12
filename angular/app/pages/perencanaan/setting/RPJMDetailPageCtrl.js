bf32198d420bd5d78258d29841c4f22e394190a7
Kadek Juliana Parwanta <kadek.parwanta@gmail.com>
2017-03-12 16:17:43 +0800

    Create modal for new bidang

--- angular/app/pages/perencanaan/setting/RPJMDetailPageCtrl.js
+++ angular/app/pages/perencanaan/setting/RPJMDetailPageCtrl.js
@@ -6,36 +6,84 @@
   'use strict';
 
   angular.module('BlurAdmin.pages.perencanaan')
-      .controller('RPJMDetailPageCtrl', RPJMDetailPageCtrl);
+    .controller('RPJMDetailPageCtrl', RPJMDetailPageCtrl);
 
   /** @ngInject */
-  function RPJMDetailPageCtrl(RPJM, $scope, rpjmId, $state) {
+  function RPJMDetailPageCtrl(RPJM, $scope, rpjmId, $state, $uibModal, Bidang) {
     var vm = this;
     $scope.rpjm = {};
-    console.log("rpjmId= " + rpjmId);
+    $scope.BidangList = [];
+    $scope.isNewRPJM = false;
 
-    if (rpjmId !== "0") {
-      RPJM.findById({id : rpjmId}, function(rpjm){
-        $scope.rpjm = rpjm;
-      })
+    function init() {
+      if (rpjmId !== "0") {
+        RPJM.findById({ id: rpjmId, filter : { include: "Bidang" }}, function (rpjm) {
+          $scope.rpjm = rpjm;
+          $scope.BidangList = rpjm.Bidang;
+        })
+      } else {
+        $scope.isNewRPJM = true;
+      }
     }
 
-    $scope.save = function() {
-      if (rpjmId === "0") {
-        RPJM.create($scope.rpjm, function(rpjm){
+    init()
+
+    $scope.save = function () {
+      if ($scope.isNewRPJM) {
+        RPJM.create($scope.rpjm, function (rpjm) {
           $state.go('perencanaan.setting');
-        }, function(err){
+        }, function (err) {
           alert(err.data.error.message);
         })
       } else {
-        RPJM.prototype$updateAttributes($scope.rpjm, function(rpjm){
+        RPJM.prototype$updateAttributes($scope.rpjm, function (rpjm) {
           $state.go('perencanaan.setting');
-        }, function(err){
+        }, function (err) {
           alert(err.data.error.message);
         })
       }
     }
 
+
+    $scope.open = function (page, size) {
+      var modalInstance = $uibModal.open({
+        animation: true,
+        templateUrl: page,
+        size: size,
+        controller: RpjmModalInstanceCtrl,
+        controllerAs: 'vm'
+      });
+
+      modalInstance.result.then(function (data) {
+        if (!$scope.isNewRPJM && !data.id) {
+          Bidang.create({
+            No:data.No,
+            Nama:data.Nama,
+            RPJMId:rpjmId
+          }, function(bidang){
+            $scope.BidangList.push(bidang);
+          })
+        }
+        
+      })
+    };
+
+  }
+
+  angular.module('BlurAdmin.pages.perencanaan')
+    .controller('RpjmModalInstanceCtrl', RpjmModalInstanceCtrl);
+
+  function RpjmModalInstanceCtrl($uibModalInstance) {
+    var vm = this;
+    vm.Bidang;
+
+    vm.ok = function () {
+      $uibModalInstance.close(vm.Bidang);
+    }
+
+    vm.cancel = function () {
+      $uibModalInstance.dismiss('cancel');
+    }
   }
 
 })();
