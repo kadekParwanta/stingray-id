148141448ee37ee27e54094be4bc033a515fe3c8
Kadek Juliana Parwanta <kadek.parwanta@gmail.com>
2017-05-04 10:24:42 +0800

    export excel draft

--- angular/app/pages/perencanaan/rpjmdes/RpjmdesPageCtrl.js
+++ angular/app/pages/perencanaan/rpjmdes/RpjmdesPageCtrl.js
@@ -426,6 +426,22 @@
         vm.convertAlphabetical = function(n) {
             return String.fromCharCode(97 + n);
         }
+
+        vm.tableToExcel = function (table, name) {
+            // var uri = 'data:application/vnd.ms-excel;base64,'
+            //     , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'
+            //     , base64 = function (s) { return window.btoa(unescape(encodeURIComponent(s))) }
+            //     , format = function (s, c) { return s.replace(/{(\w+)}/g, function (m, p) { return c[p]; }) }
+            // if (!table.nodeType) table = document.getElementById(table)
+            //     var ctx = { worksheet: name || 'Worksheet', table: table.innerHTML }
+            //     return window.location.href = uri + base64(format(template, ctx))
+            
+            var element = angular.element('#'+table);
+            var newWin = window.open("");
+            newWin.document.write(element[0].outerHTML);
+            newWin.print();
+            newWin.close();
+        }
     }
 
 })();
